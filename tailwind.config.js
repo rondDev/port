aab2623db4385926397d053d48f241be045eed80
rondDev <contact@rond.cc>
2024-01-29 13:57:04 +0100

    fix(tailwind): fix so it extends instead of replaces

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,15 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    colors: {
-      textColor: '#efd3fe',
-      backgroundColor: '#160026',
-      primaryColor: '#c36cfd',
-      secondaryColor: '#a80276',
-      accentColor: '#fc2b9d',
+    extend: {
+      colors: {
+        textColor: '#efd3fe',
+        backgroundColor: '#160026',
+        primaryColor: '#c36cfd',
+        secondaryColor: '#a80276',
+        accentColor: '#fc2b9d',
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
