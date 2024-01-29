363ee3d2f886c1ed5a8432652ee42a8e917eb24a
rondDev <contact@rond.cc>
2024-01-29 13:57:28 +0100

    fix(main page): make examples a bit more believable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,34 +5,34 @@
     {
       image: {
         link: "https://images.unsplash.com/photo-1505533321630-975218a5f66f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        alt: "kdsjfsdl",
+        alt: "Picture of the sky",
       },
-      title: "jlsfjls",
-      time: new Date(),
-      content: "lkijsdfljs",
+      title: "Project Code",
+      time: new Date(2023, 10, 27),
+      content: "This is a walkthrough of blahblah",
     },
     {
       image: {
         link: "https://images.unsplash.com/photo-1505533321630-975218a5f66f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        alt: "kdsjfsdl",
+        alt: "Clouds in the sky",
       },
-      title: "jlsfjls",
+      title: "Super Project",
       time: new Date(),
-      content: "lkijsdfljs",
+      content: "Showcasing Super blahblah",
     },
     {
       image: {
         link: "https://images.unsplash.com/photo-1505533321630-975218a5f66f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        alt: "kdsjfsdl",
+        alt: "A wonderful cloud",
       },
-      title: "jlsfjls",
-      time: new Date(),
-      content: "lkijsdfljs",
+      title: "Very Interesting Project",
+      time: new Date(2024, 0, 10),
+      content: "Interesting project description",
     },
   ];
 </script>
 
-<div class="h-full w-full text-gray-200">
+<div class="h-full w-full text-gray-200 flex flex-col items-center">
   <Header />
   <div class="flex flex-col gap-12">
     {#each projects as project}
