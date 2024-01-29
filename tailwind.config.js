531e0457ad60d033ae20715598e526f41981e3d5
rondDev <contact@rond.cc>
2024-01-29 13:29:21 +0100

    feat(tailwind): add custom tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    colors: {
+      textColor: '#efd3fe',
+      backgroundColor: '#160026',
+      primaryColor: '#c36cfd',
+      secondaryColor: '#a80276',
+      accentColor: '#fc2b9d',
+    },
     extend: {},
   },
   plugins: [],
