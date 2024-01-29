462c4865fc63fcf9595d78a462757e3f628398d4
rondDev <contact@rond.cc>
2024-01-29 13:29:01 +0100

    fix(header): add styling and correct urls to header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,8 +1,8 @@
-<div class="w-full flex items-center m-4 bg-gray-800">
-  <h1 class="grow text-[4vw]">Portfolio</h1>
-  <div class="flex gap-4 mr-8 text-[2.5vw]">
-    <a href="/">About</a>
-    <a href="/">Projects</a>
-    <a href="/">Contact</a>
+<div class="w-[85vw] flex items-center m-12 bg-primaryColor rounded-full">
+  <a href="/" class="ml-4 mr-auto text-[4vw]">Portfolio</a>
+  <div class="flex gap-8 mr-8 text-[2.5vw] text-gray-500">
+    <a href="/about" class="hover:text-gray-200">/about</a>
+    <a href="/projects" class="hover:text-gray-200">/projects</a>
+    <a href="/contact" class="hover:text-gray-200">/contact</a>
   </div>
 </div>
