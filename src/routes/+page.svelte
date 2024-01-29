2121c481e7e832db790577edbb162f1fc20ed4df
rondDev <contact@rond.cc>
2024-01-29 13:29:37 +0100

    feat(page): add proper example for project card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,47 @@
 <script lang="ts">
   import Header from "$lib/components/Header.svelte";
   import ProjectCard from "$lib/components/ProjectCard.svelte";
+  let projects = [
+    {
+      image: {
+        link: "https://images.unsplash.com/photo-1505533321630-975218a5f66f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        alt: "kdsjfsdl",
+      },
+      title: "jlsfjls",
+      time: new Date(),
+      content: "lkijsdfljs",
+    },
+    {
+      image: {
+        link: "https://images.unsplash.com/photo-1505533321630-975218a5f66f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        alt: "kdsjfsdl",
+      },
+      title: "jlsfjls",
+      time: new Date(),
+      content: "lkijsdfljs",
+    },
+    {
+      image: {
+        link: "https://images.unsplash.com/photo-1505533321630-975218a5f66f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        alt: "kdsjfsdl",
+      },
+      title: "jlsfjls",
+      time: new Date(),
+      content: "lkijsdfljs",
+    },
+  ];
 </script>
 
 <div class="h-full w-full text-gray-200">
   <Header />
-  <ProjectCard
-    image={{
-      link: "https://images.unsplash.com/photo-1505533321630-975218a5f66f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      alt: "Picture of the sky",
-    }}
-    title="Interesting new project"
-    time={new Date(2024, 0, 23)}
-    content={"Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis."}
-  />
+  <div class="flex flex-col gap-12">
+    {#each projects as project}
+      <ProjectCard
+        image={project.image}
+        title={project.title}
+        time={project.time}
+        content={project.content}
+      />
+    {/each}
+  </div>
 </div>
