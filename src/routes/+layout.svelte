9b46b21fcc19a4782e2f31021c79c15a25ee17a4
rondDev <contact@rond.cc>
2024-01-29 13:30:01 +0100

    fix(layout): change styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,8 @@
   import "../app.css";
 </script>
 
-<div class="bg-gray-900 w-[100vw] h-[100vh] overflow-hidden">
+<div
+  class="bg-backgroundColor text-textColor w-[100vw] h-[100vh] overflow-hidden select-none"
+>
   <slot />
 </div>
