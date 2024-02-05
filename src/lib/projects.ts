0a00b8fb0938fd1b8daf99c09682957c56aff88e
rondDev <contact@rond.cc>
2024-02-05 12:44:53 +0100

    fix: move logic for projects to lib file

---
+++ src/lib/projects.ts
@@ -0,0 +1,23 @@
+
+let projects = [
+    {
+        image: {
+            link: "https://images.unsplash.com/photo-1505533321630-975218a5f66f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "Picture of the sky",
+        },
+        title: "Image Uploader",
+        time: new Date(2023, 10, 27),
+        content: "I was tired of having a php script handling all my screenshot needs, so I developed a full solution to image/video uploads for my use case.",
+    },
+    {
+        image: {
+            link: "https://images.unsplash.com/photo-1505533321630-975218a5f66f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "Clouds in the sky",
+        },
+        title: "Super Project",
+        time: new Date(),
+        content: "Showcasing Super blahblah",
+    },
+];
+
+export default projects;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,10 @@
 <script lang="ts">
   import Header from "$lib/components/Header.svelte";
   import ProjectCard from "$lib/components/ProjectCard.svelte";
-  let projects = [
-    {
-      image: {
-        link: "https://images.unsplash.com/photo-1505533321630-975218a5f66f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        alt: "Picture of the sky",
-      },
-      title: "Project Code",
-      time: new Date(2023, 10, 27),
-      content: "This is a walkthrough of blahblah",
-    },
-    {
-      image: {
-        link: "https://images.unsplash.com/photo-1505533321630-975218a5f66f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        alt: "Clouds in the sky",
-      },
-      title: "Super Project",
-      time: new Date(),
-      content: "Showcasing Super blahblah",
-    },
-    {
-      image: {
-        link: "https://images.unsplash.com/photo-1505533321630-975218a5f66f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        alt: "A wonderful cloud",
-      },
-      title: "Very Interesting Project",
-      time: new Date(2024, 0, 10),
-      content: "Interesting project description",
-    },
-  ];
+import { default as projects } from '$lib/projects'
 </script>
 
-<div class="h-full w-full text-gray-200 flex flex-col items-center">
+<div class="h-full w-full text-gray-200 flex flex-col items-center overflow-x-scroll pb-8">
   <Header />
   <div class="flex flex-col gap-12">
     {#each projects as project}
