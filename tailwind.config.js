f86e4b41bac7663eb75c528be18c7c91418ee25e
rond <contact@rond.cc>
2024-02-08 15:24:10 +0100

    feat: css for component library

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
       colors: {
@@ -9,8 +9,21 @@ export default {
         primaryColor: '#c36cfd',
         secondaryColor: '#a80276',
         accentColor: '#fc2b9d',
+        // pink
+        primary: {
+          "50": "#fdf2f8",
+          "100": "#fce7f3",
+          "200": "#fbcfe8",
+          "300": "#f9a8d4",
+          "400": "#f472b6",
+          "500": "#ec4899",
+          "600": "#db2777",
+          "700": "#be185d",
+          "800": "#9d174d",
+          "900": "#831843"
+        }
       },
     },
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 };
