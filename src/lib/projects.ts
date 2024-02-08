9415294bfedc79db08600acfbf8494bb978705e1
rond <contact@rond.cc>
2024-02-08 15:31:25 +0100

    fix: formatting

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,23 +1,26 @@
+// NOTE: the id property is a string so in the future I'm able to
+//       change it if i feel like using more declarative ids
 const projects = [
     {
-        id: "0",
+        id: '0',
         image: {
-            link: "https://images.unsplash.com/photo-1505533321630-975218a5f66f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            alt: "Picture of the sky",
+            link: 'https://i.rond.cc/api/FCeGhfTM0OJC.png',
+            alt: 'Picture of the sky',
         },
-        title: "Image Uploader",
-        time: new Date(2023, 10, 27),
-        content: "I was tired of having a php script handling all my screenshot needs, so I developed a full solution to image/video uploads for my use case.",
+        title: 'Image Uploader',
+        time: new Date(2023, 8),
+        content:
+        'I was tired of having a php script handling all my screenshot needs, so I developed a full solution to image/video uploads for my use case.',
     },
     {
-        id: "1",
+        id: '1',
         image: {
-            link: "https://images.unsplash.com/photo-1505533321630-975218a5f66f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            alt: "Clouds in the sky",
+            link: 'https://images.unsplash.com/photo-1505533321630-975218a5f66f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            alt: 'Clouds in the sky',
         },
-        title: "Super Project",
+        title: 'Super Project',
         time: new Date(),
-        content: "Showcasing Super blahblah",
+        content: 'Showcasing Super blahblah',
     },
 ];
 
--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,14 +1,11 @@
 <script>
-	import { page } from '$app/stores';
-    import { GradientButton, Card } from 'flowbite-svelte';
+import { page } from '$app/stores';
+import { GradientButton, Card } from 'flowbite-svelte';
 </script>
 
 <div class="w-full h-full flex items-center justify-center gap-4">
     <Card class="bg-backgroundColor border-none min-w-96 min-h-48 flex flex-col gap-16 items-center justify-center">
-    <h1 class="text-4xl text-white">{$page.error.message}</h1>
-    <!-- svelte-ignore missing-declaration -->
-    <GradientButton shadow color="pinkToOrange" class="text-2xl"><a href="/">Go to home</a></GradientButton>
-
+        <h1 class="text-4xl text-white">{$page.error.message}</h1>
+        <GradientButton shadow color="pinkToOrange" class="text-2xl"><a href="/">Go to home</a></GradientButton>
     </Card>
-
 </div>
