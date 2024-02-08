306a158c15bac9723be138777481323ea75cc48e
rond <contact@rond.cc>
2024-02-08 15:26:52 +0100

    feat: ground work for self-healing project URLs

--- src/routes/projects/[project]/+page.ts
+++ src/routes/projects/[project]/+page.ts
@@ -1,10 +1,23 @@
-import { redirect } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageLoad} */
 export function load({ params }) {
   console.info(params.project);
   const splitArr = params.project.split('-');
   console.info(splitArr[splitArr.length - 1]);
-  //TODO: Make this functional, probably stored in projects array
-  redirect(302, '/projects/image-uploader');
+    if(splitArr.length < 1) { error(404, "/404"); }
+
+    switch(splitArr[splitArr.length - 1]) {
+        case "0": {
+            redirect(302, '/projects/image-uploader');
+            break;
+        }
+        default: {
+                // redirect(306, '/');
+            error(404, {
+                message: "Couldn't find this project"
+            })
+            break;
+        }
+    }
 }
