c4b6870fd3a0bb5737235f49017e6c97388641ae
rond <contact@rond.cc>
2024-02-08 15:24:48 +0100

    feat: rework header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,60 +1,43 @@
 <script lang="ts">
-import { onMount } from 'svelte';
+    import { onMount } from "svelte";
 
     let url = ``;
 
-     onMount(() => url = window.location.href);
-
-    function checkContains(input: string) {
-        return input.includes(input);
-    }
-let h;
-let w;
-let ow;
-let oh;
+    onMount(() => (url = window.location.href));
 </script>
 
-<div bind:offsetWidth={ow} bind:offsetHeight={oh} bind:clientWidth={w} bind:clientHeight={h} class="w-[70vw] h-[7vw] flex items-center m-12 bg-accentColor rounded-full z-10">
-  <a
-    href="/"
-    class="ml-4 mr-auto text-[4vw] hover:text-gray-700 hover:translate-y-[-0.25rem] transition-transform hover:scale-110"
-    >Portfolio</a
-  >
-  <div class="flex gap-8 mr-8 text-[2.5vw]">
+<div class="w-[100vw] h-[7vw] flex items-center mb-12 bg-accentColor z-10">
     <a
-      href="/about"
-      class="hover:text-gray-700 hover:translate-y-[-0.25rem] transition-transform hover:scale-110"
-      >/about</a
+        href="/"
+        class="ml-4 mr-auto text-[4vw] hover:text-gray-700 hover:translate-y-[-0.25rem] transition-transform hover:scale-110"
+        >Portfolio</a
     >
-    <a
-      href="/projects"
-      class="hover:text-gray-700 hover:translate-y-[-0.25rem] transition-transform hover:scale-110"
-      >/projects</a
-    >
-    <a
-      href="/contact"
-      class="hover:text-gray-700 hover:translate-y-[-0.25rem] transition-transform hover:scale-110"
-      >/contact</a
-    >
-  </div>
-  {#if url.includes("/about")}
-      <div class="relative z-0">
-          <div class={`p-2 absolute bg-red-200 text-red-900 rounded-b-lg whitespace-nowrap z-0 text-[2vw]`} style="top: {h / 2}px; right: {w/18}px">
-              You are currently at /about
-           </div>
-</div>  {/if}
-  {#if url.includes("/projects")}
-      <div class="relative z-0">
-          <div class={`p-2 absolute bg-red-200 text-red-900 rounded-b-lg whitespace-nowrap z-0 text-[2vw]`} style="top: {h / 2}px; right: {w/18}px">
-              You are currently at /projects
-           </div>
-</div>  {/if}
-  {#if url.includes("/contact")}
-      <div class="relative z-0">
-          <div class={`p-2 absolute bg-red-200 text-red-900 rounded-b-lg whitespace-nowrap z-0 text-[2vw]`} style="top: {h / 2}px; right: {w/18}px">
-              You are currently at /contact
-           </div>
-</div>  {/if}
+    <div class="flex gap-8 mr-8 text-[2.5vw] h-full items-center">
+        <a
+            href="/about"
+            class={`hover:text-gray-700 hover:translate-y-[-0.25rem] transition-transform hover:scale-110 ${
+                url.includes("/about")
+                    ? "translate-y-[-0.25rem] scale-105 text-green-400"
+                    : ""
+            }`}>/about</a
+        >
+        <a
+            href="/projects"
+            class={`hover:text-gray-700 hover:translate-y-[-0.25rem] transition-transform hover:scale-110 ${
+                url.includes("/projects")
+                    ? "translate-y-[-0.25rem] scale-105 text-green-400"
+                    : ""
+            }`}>/projects</a
+        >
+        <a
+            href="/contact"
+            class={`hover:text-gray-700 hover:translate-y-[-0.25rem] transition-transform hover:scale-110 ${
+                url.includes("/contact")
+                    ? "translate-y-[-0.25rem] scale-105 text-green-400"
+                    : ""
+            }`}>/contact</a
+        >
+    </div>
 </div>
 
 <style>
