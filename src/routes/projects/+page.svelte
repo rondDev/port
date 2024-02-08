857e26b02d338b5dbc41832a7a7b7db243ef23ca
rond <contact@rond.cc>
2024-02-08 15:27:44 +0100

    feat: move projects to /projects

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
-    import Header from "$lib/components/Header.svelte";
+  import Header from "$lib/components/Header.svelte";
+  import ProjectCard from "$lib/components/ProjectCard.svelte";
+import { projects } from '$lib/projects'
 </script>
 
-<div class="flex flex-col items-center">
-    <Header />
+<div class="h-full w-full text-gray-200 flex flex-col items-center overflow-x-scroll pb-8">
+  <Header />
+  <div class="flex flex-col gap-12">
+    {#each projects as project}
+      <ProjectCard
+        id={project.id}
+        image={project.image}
+        title={project.title}
+        time={project.time}
+        content={project.content}
+      />
+    {/each}
+  </div>
 </div>
