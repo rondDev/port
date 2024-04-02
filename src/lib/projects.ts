7210d55777c19c1ef7612663f95d65aa0f3ad335
rond <contact@rond.cc>
2024-04-02 10:21:41 +0200

    fix: remove example project

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -11,17 +11,7 @@ const projects = [
         time: new Date(2023, 8),
         content:
         'I was tired of having a php script handling all my screenshot needs, so I developed a full solution to image/video uploads for my use case.',
-    },
-    {
-        id: '1',
-        image: {
-            link: 'https://images.unsplash.com/photo-1505533321630-975218a5f66f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            alt: 'Clouds in the sky',
-        },
-        title: 'Super Project',
-        time: new Date(),
-        content: 'Showcasing Super blahblah',
-    },
+    }
 ];
 
 function getProjectById(id: string) {
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
     import Header from "$lib/components/Header.svelte";
+    import { Card } from "flowbite-svelte";
 </script>
 
 <div class="h-full w-full flex flex-col items-center">
     <Header />
-    <div class="h-full w-full"></div>
+    <div class="h-full w-full flex justify-center items-center">
+        <Card>
+            <h5 class="text-3xl text-white mb-2">Contact me</h5>
+            <p class="mb-2">Discord: @rond</p>
+            <p>Email: contact(at)rond.cc</p>
+        </Card>
+    </div>
 </div>
